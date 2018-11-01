18e33d9b6f127b8113d911b7bc12b93fe276367b
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-11-01 20:26:14 +0100

    UIController updated

--- app.js
+++ app.js
@@ -11,14 +11,41 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-   
+    // private
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    // public
+    return { 
+        getInputValue : function() {
+            return {
+                type : document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+    
+        }
+    }
+     
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var inputData = UICtrl.getInputValue();
+        console.log(inputData);
 
         // 2. Add the item to the budget controller
 
@@ -28,11 +55,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
         
-        console.log('the controller ctrlAddItem works!');
     }
 
     // in this case the ctrlAddItem function is a CALLBACK, so it does not needs parenthesis
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     // set up Event listeners for keypress events
     document.addEventListener('keypress', function(event) {
