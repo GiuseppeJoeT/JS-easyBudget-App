8076639298d92e183002bdd4918f8f8c931f7583
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-11-12 11:13:32 +0100

    UIController updated
    displayMonth() method

--- docs/app.js
+++ docs/app.js
@@ -181,7 +181,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     }
 
     return { 
@@ -275,6 +276,18 @@ var UIController = (function() {
                 }
             })
 
+        },
+
+        displayMonth: function() {
+            var now, months, month, year;
+
+            now = new Date();
+
+            months = ['January',  'February' , 'March' , 'April' , 'May' , 'June' , 'July' , 'August' , 'September' , 'October' , 'November' , 'December'];
+
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
         },
 
         getDOMstrings: function() {
@@ -389,6 +402,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('The easyBudget Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
